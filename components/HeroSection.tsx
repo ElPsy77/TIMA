ef6c9a509d2e791895e6998afa0db4e171a05670
ElPsy77 <makjikkarimbaev@gmail.com>
2025-05-25 22:18:25 +0500

    переведена herosection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -17,24 +17,23 @@ export default function HeroSection(
           <div className="flex flex-col items-start justify-between sm:flex-row sm:justify-between sm:items-center ">
             <div className="lg:max-w-[530px] sm:max-w-[400px] min-w-[100px]  sm:min-h-[350px] text-lg sm:mr-[15px] md:mr-[30px] mb-5 sm:mb-0">
               <p className=" mb-5 text-[32px] sm:text-[37px] lg:text-[55px] md:text-[42px] leading-[112.52%] font-semibold font-poppins text-secondary1 block min-h-[78px] sm:min-h-[83px] md:min-h-[101px] lg:min-h-[146px]">
-                Online{" "}
+                Онлайн{" "}
                 <span className="text-custom-primary">
-                  Learning you can access
+                Обучение Безопасности
                 </span>{" "}
-                anywhere easily!
+                в любое время и в любом месте
               </p>
               <p className=" my-[20px] max-w-[320px] md:max-w-none md:mb-[32px] text-[15px] font-light leading-[129%] sm:text-[18px] font-roboto ">
-                Discover a new way to communicate & connect with fast, easy &
-                unlimited free chat today!
+                Откройте для себя интерактивные курсы по безопасности, которые помогут вам развить навыки и уверенность в защите себя и окружающих. Наши курсы охватывают широкий спектр тем, от первой помощи до личной безопасности, и подходят для всех уровней подготовки.
               </p>
               {!userId &&  <Link href={"/auth/register"}>
                 <Button variant="default" size="custom">
-                  Get Started
+                 Начать
                 </Button>
               </Link>}
              {userId &&  <Link href={"/dashboard"}>
                 <Button variant="default" size="custom">
-                  Get Started
+                  Начать
                 </Button>
               </Link>}
             </div>
